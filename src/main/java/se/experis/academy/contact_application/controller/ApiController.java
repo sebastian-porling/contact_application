da5b62e1aba6c47b9bb67f51fc68e540049ae86c
Sebastian Porling <sebastian.porling@gmail.com>
2020-08-07 19:13:23 +0200

    Update: Documentation and new search

--- src/main/java/se/experis/academy/contact_application/controller/ApiController.java
+++ src/main/java/se/experis/academy/contact_application/controller/ApiController.java
@@ -7,6 +7,9 @@ import se.experis.academy.contact_application.repository.ContactRepository;
 
 import java.util.List;
 
+/**
+ * The Rest api controller
+ */
 @RestController
 @RequestMapping("/api")
 public class ApiController {
@@ -14,6 +17,10 @@ public class ApiController {
     @Autowired
     ContactRepository contactRepository;
 
+    /**
+     * Gets all contacts
+     * @return contacts in a list
+     */
     @GetMapping("/contacts")
     public List<Contact> getContacts() {
         return contactRepository.findByActiveTrue();
@@ -28,19 +35,34 @@ public class ApiController {
         }
     }
 
+    /**
+     * Adds a contact to the database
+     * @param contact
+     * @return The contact if succesfull
+     */
     @PutMapping("/contact/create")
     public Contact addContact(@RequestBody Contact contact) {
         return contactRepository.save(contact);
     }
 
+    /**
+     * Updates the contact
+     * @param contact
+     * @return the updated contact if successfull
+     */
     @PutMapping("/contact/update")
     public Contact updateContact(@RequestBody Contact contact) {
         if (contactRepository.existsById(contact.getId())) return contactRepository.save(contact);
         return new Contact();
     }
 
+    /**
+     * Finds contacts that matches the name, email or/and email.
+     * @param query search string
+     * @return list of contacts
+     */
     @GetMapping("/contact/search")
     public List<Contact> search(@RequestParam("query") String query) {
-        return contactRepository.findAllByActiveTrueAndNameContainingIgnoreCaseOrEmailContainingIgnoreCaseOrContactNumberContainingIgnoreCase(query, query, query);
+        return contactRepository.search(query.toLowerCase());
     }
 }
